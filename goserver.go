f5b4beae0703801998afddac49a0d4817214face
Bhanu Praneeth <72258505+RAVAN0407@users.noreply.github.com>
2022-03-10 17:59:35 +0530

    Create goserver.go

---
+++ goserver.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os/exec"
+	"strings"
+)
+
+const portNumber = ":8080"
+
+var kernel_v []byte
+var cpu []string
+
+func HomePageHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "kernel : ")
+	w.Write([]byte(kernel_v))
+	w.Write([]byte(cpu[12]))
+	fmt.Fprintf(w, "\n")
+	w.Write([]byte(cpu[4]))
+}
+
+func main() {
+
+	cmd1 := "uname"
+	cmd2 := "-r"
+	kernel_v, _ = exec.Command(cmd1, cmd2).Output()
+	//fmt.Println(string(kernel_v))
+	cmd3 := "cat"
+	cmd4 := "/proc/cpuinfo"
+	cpuinfo, _ := exec.Command(cmd3, cmd4).Output()
+	cpu = strings.Split(string(cpuinfo), "\n")
+	//fmt.Println(cpu[12])
+	//fmt.Println(cpu[4])
+
+	http.HandleFunc("/", HomePageHandler)
+	fmt.Printf("Starting application on port %v\n", portNumber)
+	http.ListenAndServe(portNumber, nil)
+}
